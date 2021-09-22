2042b8ec87f4f2210cc1ac5587738c8ff4e77fd6
rebootinnovations <91179342+rebootinnovations@users.noreply.github.com>
2021-09-22 12:39:29 +0530

    Web

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reboot Innovations (OPC) Private Limited</title>
+</head>
+
+<body>
+    <div 
+        style="
+        margin-top: 36vh;
+        display: flex;
+    align-items: center;
+    justify-content: center;
+        width:100%;font-family:-apple-system,BlinkMacSystemFont,'Segoe UI','Roboto','Oxygen','Ubuntu','Cantarell','Fira Sans','Droid Sans','Helvetica Neue',sans-serif">
+        <div align="center"><img src="https://github.com/rebootinnovations/statics/raw/main/reboot-logo.png"
+                width="64" height="64" class="CToWUd">
+            <div style="padding-left:10px;font-weight:300;font-size:27px">REBOOT INNOVATIONS (OPC) PRIVATE LIMITED
+                <br>
+                <span style="font-weight:400;font-size:13px"><span style="font-weight:400;font-size:13px"><span
+                            style="font-weight:500">Web: </span><a href="https://www.rebootinnovations.com"
+                            target="_blank">www.rebootinnovations.com</a> &bull; <span
+                            style="font-weight:400;font-size:13px"><span style="font-weight:500">E-mail: </span><a
+                                href="mailto:info@rebootinnovations.com" target="_blank"><span
+                                    class="il">info@rebootinnovations.com</span></a><br></span></span></span>
+                <div style="font-weight:400;font-size:14px">1st Flr A Square, Building, Edathala P.O.,
+                    Kuzhuvelippady<br> Ernakulam, Kerala, India - 683561</div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
